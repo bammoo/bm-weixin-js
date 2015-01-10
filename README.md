0a80bf333ed7932c4a1fdfffc668f337f458f1d1
john <johnxiao@ifanr.com>
2015-01-10 12:00:32 +0800

    udp readme

--- README.md
+++ README.md
@@ -1,18 +1,28 @@
-bm-weixin
+bm-weixin 微信 2015-1-9 新 js sdk 封装
 ===============
 
-微信分享 js 封装
+## 微信分享 js 封装
 
-## example
+### example
 ```javascript
 
   // 分享
   ns.setWeixinShare({
-    pic: 'https://avatars3.githubusercontent.com/u/5893693?v=3&s=460',
+    imgUrl: 'https://avatars3.githubusercontent.com/u/5893693?v=3&s=460',
     link: 'https://github.com/bammoo',
     title: 'bammoo\'s github',
-    callback: function() {
-      // cb after click share menu
+    success: function() {
+      // 成功后回调
     }
+  })
+```
+
+## 微信获取网络状态接口 js 封装
+
+### example
+```javascript
+  ns.getNetworkType(function(networkType) {
+      // networkType 取值为 2g，3g，4g，wifi
+      // 成功后回调
   })
 ```
--- bm-weixin.js
+++ bm-weixin.js
@@ -1,102 +1,29 @@
 var ns = ns || {};
 
-var _shareCallback = function() {};
-
 var _shareData = {
-  "img_url": '',
-  "link": window.location.href,
+  "imgUrl": '',
+  "link": '',
   "desc": '',
-  "title": document.title
-}
-
-// share timeline 取消时返回了 ok
-var parseRes = function(resp) {
-  if(!resp || !resp.err_msg)
-   return false;
-  var re = resp.err_msg.split(':');
-  if(!re.length)
-    return;
-  // 有时是 confirm 有时是 ok
-  if(re[1] == 'confirm')
-    re[1] = 'ok';
-  return {
-    'status': re[1],
-    'type': re[0]
-  }
+  "title": '',
+  success: function() {},
+  cancel: function() {}
 }
 
 var _weixinShareCall = function() {
-
-  // 发送给朋友
-  WeixinJSBridge.on('menu:share:appmessage', function(argv){
-    // alert(_shareData.img_url);
-    WeixinJSBridge.invoke('sendAppMessage', _shareData, function(res) {
-      // alert(parseRes(res)[0] + ', ' + parseRes(res)[1]);
-      _shareCallback && _shareCallback(parseRes(res));
-    });
-  });
-
-  // 发送到朋友圈
-  WeixinJSBridge.on('menu:share:timeline', function(argv){
-    WeixinJSBridge.invoke('shareTimeline', _shareData, function(res){
-      _shareCallback && _shareCallback(parseRes(res));
-    });
-  });
-
-}
-
-/**
- *
- * @param  {Function} callback [description]
- * @return {[type]}            [description]
- */
-window.getWeixinNetworkType = function(callback) {
-  if (callback && typeof callback === 'function') {
-    ns.weixinJSBridgeInitCallback(function() {
-
-      WeixinJSBridge.invoke('getNetworkType', {}, function (resp) {
-        /**
-         *
-         * 在这里拿到 err_msg，这里面就包含了所有的网络类型
-         *
-         * network_type:wifi     wifi网络
-         * network_type:edge     非wifi,包含3G/2G
-         * network_type:fail     网络断开连接
-         * network_type:wwan     2g或者3g
-         */
-        var re = resp.err_msg.split(':');
-        if(re[1] != 'wifi') {
-          re[1] = '3g';
-        }
-        else {
-          re[1] = 'wifi';
-        }
-        callback(re[1]);
-
-      });
-
-    });
-  }
-}
-
-
-ns.weixinJSBridgeInitCallback = function(callback) {
-  // already has WeixinJSBridge
-  if(typeof WeixinJSBridge !== 'undefined') {
-    callback();
-  }
-  // 当微信内置浏览器完成内部初始化后会触发 WeixinJSBridgeReady 事件。
-  else {
-    if(document.addEventListener){
-      document.addEventListener('WeixinJSBridgeReady', callback, false);
-    }else if(document.attachEvent){
-      document.attachEvent('WeixinJSBridgeReady'   , callback);
-      document.attachEvent('onWeixinJSBridgeReady' , callback);
-    }
-  }
+  wx.onMenuShareAppMessage(_shareData);
+  wx.onMenuShareTimeline(_shareData);
 }
 
 var _weixinShareCallAttached = false;
+
+/**
+ * 清空分享数据
+ * 使用场景：spa 更新了视图，防止分享之前的视图绑定的数据
+ * @return {null}
+ */
+ns.clearWeixinShareData = function() {
+  ns.setWeixinShareData(null)
+}
 /**
  * 分享配置
  * @param {object} options 分享内容
@@ -104,12 +31,12 @@ var _weixinShareCallAttached = false;
  * @param {string} options.link 分享后点击进来的链接，如果不设置就使用 window.location.href
  * @param {string} options.title 分享出去的标题，如果不设置就使用 document.title
  */
-ns.setWeixinShare = function(options) {
+ns.setWeixinShareData = function(options) {
   options || (options = {});
+  // console.log('options:', options)
 
-  // 图片、回调函数 可以无数次替换
-  // options 为空也替换，等于重置
-  _shareData.img_url = options.pic;
+  // options.xx 为空也替换，等于清空旧数据
+  _shareData.imgUrl = options.imgUrl;
   _shareData.link = options.link;
   _shareData.link || (_shareData.link = window.location.href);
 
@@ -117,9 +44,26 @@ ns.setWeixinShare = function(options) {
   _shareData.title || (_shareData.title = document.title);
 
   // alert(_shareData.link);
-  _shareCallback = options.callback;
+  _shareData.success = options.success;
+  _shareData.cancel = options.cancel;
 
-  // _weixinShareCall 只挂一次
-  !_weixinShareCallAttached && ns.weixinJSBridgeInitCallback(_weixinShareCall);
+  // _weixinShareCall 只挂一次，以后只修改引用的参数
+  !_weixinShareCallAttached && wx.ready(_weixinShareCall);
   _weixinShareCallAttached = true;
+}
+
+/**
+ * 获取网络状态接口
+ * @param  {Function} cb 请求成功后回调
+ * @return {null}
+ */
+ns.getNetworkType = function(cb) {
+  wx.ready(function() {
+    wx.getNetworkType({
+      success: function (res) {
+        // 返回网络类型2g，3g，4g，wifi
+        cb && cb(res.networkType)
+      }
+    })
+  });
 }
